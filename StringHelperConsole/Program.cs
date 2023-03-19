368de97fc03cd60971c652ae776e8a165dbba7a3
Ricaldone Matias <mricaldone@andreani.com>
2023-03-19 00:30:26 -0300

    atress test

--- StringHelperConsole/Program.cs
+++ StringHelperConsole/Program.cs
@@ -3,10 +3,23 @@ using StringHelper;
 
 Console.WriteLine("Hello, World!");
 
+var tStart = DateTime.Now;
+
 //Arrange
 var cadenaBuscada = "puan puan";
+//var lineas = new List<string>() { "puanpuan x", "puan puan x", "puan x" };
 var lineas = new List<string>() { "puanpuan x", "puan puan x", "puan x" };
+
+for(int i = 0; i < 100000; i++)
+{
+    lineas.Add($"{i}xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx{i}");
+}
+
 //Act
 var puntaje = Matcher.BuscarMejor(cadenaBuscada, lineas);
 //Assert
 Console.WriteLine($"'{puntaje}'");
+
+var tEnd = DateTime.Now;
+
+Console.WriteLine((tEnd - tStart).TotalMilliseconds);
